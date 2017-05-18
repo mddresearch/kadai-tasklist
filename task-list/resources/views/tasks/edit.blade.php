b80be6cb3b2adb8b3a8316412c6468fd6d0ce1a7
hidebkk <mddresearch@gmail.com>
2017-05-18 13:24:34 +0000

    saiteishutsu

--- task-list/resources/views/tasks/edit.blade.php
+++ task-list/resources/views/tasks/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="row">
-        <div class="col-xs-12 col-sm-offset-2 col-sm-6 col-md-offset-2 col-md-6 col-lg-offset-3 col-lg-3">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
 
     <h1>id: {{ $task->id }} 編集ページ</h1>
 
